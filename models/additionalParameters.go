d94d8bb5e83816c519c9659d2718623ca4c55bc7
Jesper Frisk <61691900+MrDweller@users.noreply.github.com>
2024-03-06 09:08:06 +0100

    Update additionalParameters.go

--- models/additionalParameters.go
+++ models/additionalParameters.go
@@ -2,5 +2,5 @@ package models
 
 type AdditionalParametersArrowhead_4_6_1 struct {
 	OrchestrationFlags map[string]bool `json:"orchestrationFlags,omitempty"`
-	RequesterCloud     RequesterCloud  `json:"requesterCloud,omitempty"`
+	RequesterCloud     *RequesterCloud `json:"requesterCloud,omitempty"`
 }
